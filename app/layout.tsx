b254ac71efb18293d223dca7a67b12443c339681
Mohammad <mohammad.rfn98@gmail.com>
2026-05-14 20:48:34 +0330

    debug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-//
-import {AuthProvider} from './context/AuthContext'
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import "./globals.css";
+
+import {AuthProvider} from './context/AuthContext'
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +17,7 @@ export default function RootLayout({
   return (
     <html
       lang="fa" dir="rtl"
-      className={` ${geistMono.variable} h-full antialiased`}
+      className="h-full antialiased"
     >
       <body className="min-h-full flex flex-col">
        <AuthProvider>
